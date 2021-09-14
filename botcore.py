8c8531644dfe5e4b4a44a1d7fe36548b2737c499
Sovietball1.0 <77866829+Sovietball1922@users.noreply.github.com>
2021-09-15 00:25:54 +0900

    Changed to pep8 compliance

--- botcore.py
+++ botcore.py
@@ -28,16 +28,20 @@ def poston_twitter(mode: TweetMode, message: str, path=''):
         settings = SETTINGS
         with open(KEY_FILE) as raw_json:
             keys = json.load(raw_json)
-        auth = tweepy.OAuthHandler(keys['twitter']['apiKey'], keys['twitter']['apiSecret'])
-        auth.set_access_token(keys['twitter']['token'], keys['twitter']['tokenSecret'])
+        auth = tweepy.OAuthHandler(
+            keys['twitter']['apiKey'],
+            keys['twitter']['apiSecret'])
+        auth.set_access_token(
+            keys['twitter']['token'],
+            keys['twitter']['tokenSecret'])
         api = tweepy.API(auth)
-        if (settings['args']['isDebugMode'] | settings['main']['isDebugMode'] == True):
-            if (mode == TweetMode.Picture and os.path.exists(path) == True):
+        if (settings['args']['isDebugMode'] | settings['main']['isDebugMode']):
+            if (mode == TweetMode.Picture and os.path.exists(path)):
                 print(path)
             elif (message != ''):
                 if(mode == TweetMode.Text):
                     print(message)
-                elif (mode == TweetMode.TextAndPicture and os.path.exists(path) == True):
+                elif (mode == TweetMode.TextAndPicture and os.path.exists(path)):
                     print(message)
                     print(path)
             else:
@@ -53,9 +57,12 @@ def poston_twitter(mode: TweetMode, message: str, path=''):
             else:
                 raise ValueError('Tweet message is empty.')
     except FileNotFoundError as ex:
-        log_local(Result.Error, f'keys.json does not found in {CWD} or picture does not found.', ex)
+        log_local(
+            Result.Error,
+            f'keys.json does not found in {CWD} or picture does not found.',
+            ex)
     except Exception as ex:
-        log_local(Result.Error, '', ex)
+        log_local(Result.Error, excep_obj=ex)
     else:
         log_local(Result.Success, 'Tweeted successfully.')
 
@@ -63,65 +70,91 @@ def poston_twitter(mode: TweetMode, message: str, path=''):
 def select_proverb():
     try:
         settings = SETTINGS
-        if (settings['args']['isGoodmorning'] == True):
+        if (settings['args']['isGoodmorning']):
             return 'Доброе утро'
-        elif (settings['args']['isGoodnight'] == True):
+        elif (settings['args']['isGoodnight']):
             return ''
         else:
             with open(TWEETS_FILE, 'r', encoding='utf-8') as raw_json:
                 proverbs = json.load(raw_json)
                 selected = []
-                if (proverbs['russian']['isEnable'] == True):
+                if (proverbs['russian']['isEnable']):
                     if (len(proverbs['russian']['proverbs']) != 0):
                         russian = proverbs['russian']['proverbs']
-                        r = random.randint(0, len(russian)-1)
+                        r = random.randint(0, len(russian) - 1)
                         selected.append(russian[r])
                     else:
-                        raise ValueError('proverbs list is empty')
-                if (proverbs['english']['isEnable'] == True):
+                        log_local(Result.Caution, 'proverbs list is empty')
+                if (proverbs['english']['isEnable']):
                     if (len(proverbs['english']['proverbs']) != 0):
                         english = proverbs['english']['proverbs']
-                        e = random.randint(0, len(english)-1)
+                        e = random.randint(0, len(english) - 1)
                         selected.append(english[e])
                     else:
-                        raise ValueError('proverbs list is empty')
-                if (proverbs['japanese']['isEnable'] == True):
+                        log_local(Result.Caution, 'proverbs list is empty')
+                if (proverbs['japanese']['isEnable']):
                     if (len(proverbs['japanese']['proverbs']) != 0):
                         japanese = proverbs['japanese']['proverbs']
-                        j = random.randint(0, len(japanese)-1)
+                        j = random.randint(0, len(japanese) - 1)
                         selected.append(japanese[j])
                     else:
-                        raise ValueError('proverbs list is empty')
-                s = random.randint(0, len(selected)-1)
+                        log_local(Result.Caution, 'proverbs list is empty')
+                s = random.randint(0, len(selected) - 1)
                 return selected[s]
     except Exception as ex:
-        log_local(Result.Error, '', ex)
+        log_local(Result.Error, excep_obj=ex)
         return ''
 
 
 def pick_log_file():
     settings = get_settings()
-    limited_log_size = settings['log']['maxLogSize']
+    log_size_limit = settings['log']['maxLogSize']
     log_dir = settings['log']['logDirectory']
     logs = glob.glob(settings['log']['logDirectory'] + '/*.log')
     if (len(logs) > 0):
         for log in logs:
-            if (os.path.getsize(log) <= limited_log_size):
+            if (os.path.getsize(log) <= log_size_limit):
                 return log
-        return f'{log_dir}/{datetime.datetime.now().strftime("%y%m%d%H%M%S")}.log'
-    else:
-        return f'{log_dir}/{datetime.datetime.now().strftime("%y%m%d%H%M%S")}.log'
+
+    return f'{log_dir}/{datetime.datetime.now().strftime("%y%m%d%H%M%S")}.log'
 
 
-def create_log_message(result: Result, message, excep_obj=None):
+def log_local(
+        result: Result,
+        message: str = '',
+        excep_obj: Exception = None):
+    log_file = LOG_FILE
+    settings = SETTINGS
+    log_dir = str(settings['log']['logDirectory'])
+    log_message = create_log_message(result, message, excep_obj)
+    if not os.path.exists(log_file):
+        if not os.path.exists(log_dir):
+            os.mkdir(log_dir)
+        with open(log_file, 'w') as f:
+            print('', file=f, end='')
+
+    with open(log_file, mode='a') as f:
+        print(log_message, file=f, end='')
+        if (settings['args']['isShowLogOutput'] |
+                settings['main']['isShowLogOutput']):
+            print(log_message, end='')
+
+    if (result == Result.Error and settings['main']['ignoreError'] is False):
+        raise (Exception(excep_obj))
+
+
+def create_log_message(
+        result: Result,
+        message: str = '',
+        excep_obj: Exception = None):
     settings = SETTINGS
     dtformat = '%y-%m-%d-%H-%M-%S'
     if (result == Result.Error):
-        if(excep_obj == None):
+        if(excep_obj is None):
             raise ValueError('Exception object is none.')
         else:
             stacktrace = ''
-            if (settings['log']['isLogStacktrace'] == True):
+            if (settings['log']['isLogStacktrace']):
                 stacktrace = traceback.format_exc()
             if (message == ''):
                 return f'{datetime.datetime.now().strftime(str(dtformat))}  {Result.Error.value} {type(excep_obj)}{excep_obj}\n{stacktrace}'
@@ -134,26 +167,6 @@ def create_log_message(result: Result, message, excep_obj=None):
             return f'{datetime.datetime.now().strftime(str(dtformat))}  {result.value} {message}\n'
 
 
-def log_local(result: Result, message, excep_obj=None):
-    log_file = LOG_FILE
-    settings = SETTINGS
-    log_dir = str(settings['log']['logDirectory'])
-    log_message = create_log_message(result, message, excep_obj)
-    if (os.path.exists(log_file) == False):
-        if (os.path.exists(log_dir) == False):
-            os.mkdir(log_dir)
-        with open(log_file, 'w') as f:
-            print('', file=f, end='')
-
-    with open(log_file, mode='a') as f:
-        print(log_message, file=f, end='')
-        if (settings['args']['isShowLogOutput'] | settings['main']['isShowLogOutput'] == True):
-            print(log_message, end='')
-
-    if (result == Result.Error and settings['main']['ignoreError'] == False):
-        raise (excep_obj)
-
-
 def get_settings():
     settings = {}
     try:
@@ -163,7 +176,7 @@ def get_settings():
         log_local(Result.Error, f'settings.json does not found in {CWD}.', ex)
     finally:
         settings.setdefault('log', {})
-        settings['log'].setdefault('maxLogSize', 51200)  # 500KB
+        settings['log'].setdefault('maxLogSize', 51200)
         settings['log'].setdefault('logDirectory', f'{CWD}/log')
         settings['log'].setdefault('isLogStacktrace', False)
         settings.setdefault('main', {})
@@ -188,8 +201,7 @@ def parse_args():
         prog='botcore.py',
         usage='python3.9 botcore.py [-d|--debug] [settings...]',
         epilog='MIT License  Copyright (c) 2021 Семён Мошнко  GitHub: https://github.com/Sovietball1922/LeninBotCore',
-        add_help=False
-    )
+        add_help=False)
     parser.add_argument('--version', action='store_true')
     parser.add_argument('-v', '--verbose', action='store_true')
     parser.add_argument('-d', '--debug', action='store_true')
@@ -197,17 +209,13 @@ def parse_args():
     parser.add_argument('-n', '--good_night', action='store_true')
     parser.add_argument('-?', '--help', action='help')
     arg = parser.parse_args(args)
-
-    if (arg.version == True):
+    if (arg.version):
         print(f'botcore.py version: {VERSION}')
         sys.exit()
-
-    if (arg.good_morning == True and arg.good_night == True):
-        # log_local(Result.Caution, '-m and -n switch cannot use at same time')
+    if (arg.good_morning and arg.good_night):
         arg_values['args']['isDebugMode'] = arg.debug
         arg_values['args']['isShowLogOutput'] = arg.verbose
         return arg_values
-
     arg_values['args']['isDebugMode'] = arg.debug
     arg_values['args']['isShowLogOutput'] = arg.verbose
     arg_values['args']['isGoodmorning'] = arg.good_morning
@@ -216,7 +224,7 @@ def parse_args():
 
 
 CWD = os.path.dirname(__file__)
-VERSION = f'1.2.910.1718'
+VERSION = '1.2.915.14'
 KEY_FILE = f'{CWD}/keys.json'
 TWEETS_FILE = f'{CWD}/tweets.json'
 SETTINGS_FILE = f'{CWD}/settings.json'
